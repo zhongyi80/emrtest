1dbf92ef41bdbac53baab2cf5dcbe01dee88f7a7
Yi Zhong <zhongyi80@hotmail.com>
2023-04-30 16:24:38 -0400

    init dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/home/Home"
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/emrtest">
+            <Route index element={<Home /> }/>
+            {/* <Route path="login" element={<Login/>}/> */}
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
